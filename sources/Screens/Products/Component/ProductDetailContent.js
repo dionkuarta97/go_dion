5e039c4b59d6a597ab6c638981d40275949026da
Leonard Papilaya <leopapilaya@gmail.com>
2021-11-26 00:08:38 +0800

    Update ui info produk

--- sources/Screens/Products/Component/ProductDetailContent.js
+++ sources/Screens/Products/Component/ProductDetailContent.js
@@ -1,9 +1,12 @@
 import React from "react";
-import {ScrollView, StyleSheet, Text, View} from "react-native";
+import {ScrollView, StyleSheet, Text, View, TouchableOpacity, Dimensions} from "react-native";
 import Divider from "../../../Components/Divider";
 
 import Fonts from "../../../Theme/Fonts";
 import Sizes from "../../../Theme/Sizes";
+import Colors from "../../../Theme/Colors";
+
+const {width} = Dimensions.get("screen");
 
 const ProductDetailContent = (props) => {
     const item = props.item;
@@ -32,10 +35,25 @@ const ProductDetailContent = (props) => {
                     <Text>Level: {item.details.level}</Text>
                     <Divider />
                     <Text>Wilayah: {item.details.wilayah}</Text>
+                    <Divider />
+                    {item.purchased && (
+                        <TouchableOpacity
+                            activeOpacity={0.9}
+                            onPress={() => {
+                                // dispatch(addToCart(item));
+                                // navigation.navigate("CartScreen");
+                            }}
+                            style={{
+                                ...styles.button,
+                                backgroundColor: Colors.primaryColor,
+                            }}
+                        >
+                            <Text style={{...Fonts.black17Bold}}>
+                                Lanjutkan Belajar Kamu
+                            </Text>
+                        </TouchableOpacity>
+                    )}
                 </View>
-                
-
-                
             </ScrollView>
         </View>
     );
@@ -47,5 +65,13 @@ const styles = StyleSheet.create({
     content: {
         padding: Sizes.fixPadding,
         height: 1000,
+    },
+    button: {
+        paddingVertical: Sizes.fixPadding + 2.0,
+        alignItems: "center",
+        justifyContent: "center",
+        width: width - 40,
+        borderRadius: Sizes.fixPadding - 5.0,
+        marginTop: Sizes.fixPadding + 3.0,
     },
 });
