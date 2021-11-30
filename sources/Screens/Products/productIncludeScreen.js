c926b52240f39ce804c480f68e1f8e6265d4b260
Rifqi Radifan <rifqiradifan@gmail.com>
2021-11-30 13:02:48 +0700

    Perbaikan lanjutkan materi

--- sources/Screens/Products/productIncludeScreen.js
+++ sources/Screens/Products/productIncludeScreen.js
@@ -12,6 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getMateri } from "../../Redux/Materi/materiActions";
 import LoadingIndicator from "../../Components/Indicator/LoadingIndicator";
 import { getIncludesProduk } from "../../Redux/Produk/produkActions";
+import NumberFormat from "react-number-format";
 
 const ProductIncludeScreen = (props) => {
     const dispatch = useDispatch();
@@ -95,135 +96,9 @@ const ProductIncludeScreen = (props) => {
                 {includes.loading && <LoadingIndicator />}
                 {includes.data !== null && (
                     <FlatList
+                        data={includes.data}
                         keyExtractor={(item) => item._id}
-                        renderItem={({ item }) => (
-                            <TouchableOpacity
-                                onPress={() => {
-                                    navigation.navigate("ProductDetailScreen", {
-                                        item: item,
-                                        section: title,
-                                    });
-                                }}
-                            >
-                                <View
-                                    style={{
-                                        ...CompStyles.defaultCard,
-                                        marginHorizontal: Sizes.fixPadding * 1,
-                                        flexDirection: "row",
-                                    }}
-                                >
-                                    <View
-                                        style={{
-                                            width: 80,
-                                            height: 80,
-                                            borderRadius: Sizes.fixPadding,
-                                            marginRight: Sizes.fixPadding,
-                                            overflow: "hidden",
-                                            position: "relative",
-                                        }}
-                                    >
-                                        <Image
-                                            source={{ uri: item.thumbnail }}
-                                            style={{
-                                                width: "100%",
-                                                height: "100%",
-                                            }}
-                                            resizeMode="cover"
-                                        />
-                                        {item.purchased && (
-                                            <View
-                                                style={styles.purchasedCircle}
-                                            >
-                                                <MaterialIcons
-                                                    name="check"
-                                                    size={12}
-                                                    color="white"
-                                                />
-                                            </View>
-                                        )}
-                                    </View>
-                                    <View
-                                        style={{
-                                            flex: 1,
-                                            paddingVertical:
-                                                Sizes.fixPadding / 2,
-                                        }}
-                                    >
-                                        <Text
-                                            style={{
-                                                ...Fonts.orangeColor14Bold,
-                                            }}
-                                        >
-                                            {title}
-                                        </Text>
-                                        <Text
-                                            style={{
-                                                ...Fonts.black17Regular,
-                                            }}
-                                        >
-                                            {item.title}
-                                        </Text>
-                                        <View
-                                            style={{
-                                                flexDirection: "row",
-                                                alignItems: "center",
-                                            }}
-                                        >
-                                            <NumberFormat
-                                                value={
-                                                    item.price_discount !== 0
-                                                        ? item.price_discount
-                                                        : item.price
-                                                }
-                                                displayType={"text"}
-                                                thousandSeparator="."
-                                                decimalSeparator=","
-                                                prefix={"IDR "}
-                                                renderText={(value, props) => (
-                                                    <Text
-                                                        style={{
-                                                            ...Fonts.black17Bold,
-                                                            paddingVertical:
-                                                                Sizes.fixPadding /
-                                                                2,
-                                                        }}
-                                                    >
-                                                        {value}
-                                                    </Text>
-                                                )}
-                                            />
-                                            {item.price_discount > 0 && (
-                                                <NumberFormat
-                                                    value={item.price}
-                                                    displayType={"text"}
-                                                    thousandSeparator="."
-                                                    decimalSeparator=","
-                                                    prefix={"IDR "}
-                                                    renderText={(
-                                                        value,
-                                                        props
-                                                    ) => (
-                                                        <Text
-                                                            style={{
-                                                                marginLeft:
-                                                                    Sizes.fixPadding,
-                                                                color: "black",
-                                                                opacity: 0.8,
-                                                                textDecorationLine:
-                                                                    "line-through",
-                                                            }}
-                                                        >
-                                                            {value}
-                                                        </Text>
-                                                    )}
-                                                />
-                                            )}
-                                        </View>
-                                    </View>
-                                </View>
-                            </TouchableOpacity>
-                        )}
-                        data={allProduk.data}
+                        renderItem={({ item, index }) => renderItem(item)}
                     />
                 )}
             </View>
