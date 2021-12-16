b3f1616910819213e89668f81cdfa8fa7ca12d62
Dion Kuarta <ionarta@NS14A6-5e38e15e.localdomain>
2021-12-16 13:42:03 +0700

    fix edit profile 2

--- sources/Screens/Profile/profileScreen.js
+++ sources/Screens/Profile/profileScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView, StatusBar, Text, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import SliverAppBar from "../../Components/sliverAppBar";
@@ -6,11 +6,22 @@ import Colors from "../../Theme/Colors";
 import Fonts from "../../Theme/Fonts";
 import ProfileContent from "./Component/ProfileContent";
 import { useNavigation } from "@react-navigation/core";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { getListCity, getListProvince } from "../../Redux/Data/dataActions";
 const ProfileScreen = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   const profile = useSelector((state) => state.profileReducer.profile);
+  const listProvince = useSelector((state) => state.dataReducer.listProvince);
+
+  const getIdProfinsiSchool = (provinsi) => {
+    const id = listProvince.data?.filter((value) => value.provinsi === provinsi);
+    if (id !== undefined) return id[0]?.idprovinsi;
+  };
+  useEffect(() => {
+    dispatch(getListProvince());
+  }, []);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <SliverAppBar
@@ -19,11 +30,12 @@ const ProfileScreen = () => {
           <View style={{ flexDirection: "row" }}>
             <TouchableOpacity
               activeOpacity={0.6}
-              onPress={() =>
+              onPress={() => {
+                dispatch(getListCity(getIdProfinsiSchool(profile.provinsi_sekolah)));
                 navigation.navigate("ProfileEditScreen", {
                   profile: profile,
-                })
-              }
+                });
+              }}
             >
               <Text style={{ paddingHorizontal: 15, paddingVertical: 5, borderRadius: 50, backgroundColor: Colors.blackColor, color: Colors.primaryColor, fontWeight: "bold", letterSpacing: 1.2, fontSize: 14 }}>
                 <MaterialIcons size={14} name="edit" /> Edit Profile
