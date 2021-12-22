b62c7b157119add19091f0076089aec42bbc3678
Leonard Papilaya <leopapilaya@gmail.com>
2021-12-22 16:53:16 +0800

    Add 3 image banner locally

--- sources/Redux/Version/versionReducer.js
+++ sources/Redux/Version/versionReducer.js
@@ -1,6 +1,8 @@
 const initialState = {
     androidVersion: "0.1.47",
     iosVersion: "0.0.0",
+    androidCode: 27,
+    iosCode:0
 };
 
 export function versionReducer(state = initialState, action) {
