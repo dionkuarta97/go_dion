fe27575ea5b86bc8a1ce82c6f97116917331cd9b
Leonard Papilaya <leopapilaya@gmail.com>
2022-01-14 08:15:57 +0800

    Update version

--- sources/Redux/Version/versionReducer.js
+++ sources/Redux/Version/versionReducer.js
@@ -1,9 +1,9 @@
 import { SET_CHECK_VERSION } from "./versionTypes";
 
 const initialState = {
-  androidVersion: "0.1.48",
+  androidVersion: "0.1.49",
   iosVersion: "0.0.0",
-  androidCode: 28,
+  androidCode: 29,
   iosCode: 0,
   checkVersion: {
     upToDate: false,
