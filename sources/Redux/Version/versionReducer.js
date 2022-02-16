893375a24464a2c598299561af6b61ce42252112
Leonard Papilaya <leopapilaya@gmail.com>
2022-02-16 10:47:46 +0800

    Upgrade versi app

--- sources/Redux/Version/versionReducer.js
+++ sources/Redux/Version/versionReducer.js
@@ -1,9 +1,9 @@
 import { SET_CHECK_VERSION } from "./versionTypes";
 
 const initialState = {
-  androidVersion: "0.1.51",
+  androidVersion: "0.1.52",
   iosVersion: "0.0.0",
-  androidCode: 31,
+  androidCode: 32,
   iosCode: 0,
   checkVersion: {
     upToDate: false,
