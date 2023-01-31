02253131392a66abff1b50c462bd306f0508bc3f
Leo <leopapilaya@gmail.com>
2023-01-31 10:08:45 +0800

    Failed

--- android/app/src/main/java/com/goonline/app/MainActivity.java
+++ android/app/src/main/java/com/goonline/app/MainActivity.java
@@ -11,7 +11,7 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
-// import expo.modules.ReactActivityDelegateWrapper;
+import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {
 
@@ -44,11 +44,13 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
+    return new ReactActivityDelegateWrapper(
+        this,
+        new ReactActivityDelegate(this, getMainComponentName()) {
           @Override
           protected ReactRootView createRootView() {
             return new RNGestureHandlerEnabledRootView(MainActivity.this);
           }
-        };
+        });
   }
 }
