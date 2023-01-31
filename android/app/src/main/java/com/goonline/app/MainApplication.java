902323226ac549e3a21d3fd7971b5b4f433fb86b
Leo <leopapilaya@gmail.com>
2023-01-31 10:31:14 +0800

    Remove expo again

--- android/app/src/main/java/com/goonline/app/MainApplication.java
+++ android/app/src/main/java/com/goonline/app/MainApplication.java
@@ -15,15 +15,14 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import expo.modules.ApplicationLifecycleDispatcher;
-import expo.modules.ReactNativeHostWrapper;
+// import expo.modules.ApplicationLifecycleDispatcher;
+// import expo.modules.ReactNativeHostWrapper;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
-      this,
+  private final ReactNativeHost mReactNativeHost = 
       new ReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
@@ -45,7 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
           return "index";
         }
 
-      });
+      };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -58,13 +57,13 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
 
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    ApplicationLifecycleDispatcher.onApplicationCreate(this);
+    // ApplicationLifecycleDispatcher.onApplicationCreate(this);
   }
 
   @Override
   public void onConfigurationChanged(@NonNull Configuration newConfig) {
     super.onConfigurationChanged(newConfig);
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
+    // ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
   }
 
   /**
